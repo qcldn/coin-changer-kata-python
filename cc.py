a90cfe965cbd9b568a1f574c46c9b86591930abc
Daniel Irvine <daniel@danielirvine.com>
2017-05-19 16:17:44 +0100

    With looping

--- cc.py
+++ cc.py
@@ -1,10 +1,10 @@
 denominations = [200, 100, 50, 20, 10, 5, 2, 1]
 
-def change(amount, coins = denominations):
-    current_coin, *remaining_coins = coins
-    if amount == 0:
-        return []
-    elif amount >= current_coin:
-        return change(amount - current_coin, coins) + [current_coin]
-    else:
-        return change(amount, remaining_coins)
+def change(amount):
+    coins = []
+    for coin in denominations:
+        while amount >= coin:
+            coins.append(coin)
+            amount -= coin
+    coins.reverse()
+    return coins
